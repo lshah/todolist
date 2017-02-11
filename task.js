1d0495080c574c9a2ec094f7490027f02d50cbaa
lshah <1480four@gmail.com>
2017-02-11 16:32:39 -0700

    improvements

--- task.js
+++ task.js
@@ -2,18 +2,22 @@
 document.getElementById('btn').addEventListener('click', add);
 
 
+function enterEvent(event){
+	var x= event.which || event.keyCode;
+	if(x == 13){
+		add();
+	}
+}
+
 var task = [];
 var listItem = document.getElementById('todotext');
 var tasklist = document.getElementById('list');
 showList();
 
+
+
+
 function add(){
-	/**if(localStorage.getItem('task') !== null){
-		var retrieveList = localStorage.getItem('task');
-		var retrievedList = JSON.parse(retrieveList);
-		task = retrievedList;
-	}**/
-	
 	task.push(listItem.value);
 	console.log(task);
 	localStorage.setItem('task', JSON.stringify(task));
@@ -48,4 +52,8 @@ function showList(){
 	}
 	
 	
+}
+
+function clearList(){
+	window.localStorage.clear();
 }
